f4710f16bff9f75d430344b1c2c2bbd99452d992
Sebastian Held <sebastian.held@gmx.de>
2012-07-27 20:10:45 +0200

    improved parser

--- gerberimporter.cpp
+++ gerberimporter.cpp
@@ -19,57 +19,33 @@ bool GerberImporter::import( QString filename )
     QTextStream stream(&file);
     QString line;
     bool finished = false;
-    enum {readLine,process,parameterBlock} state = readLine;
+    enum {dataBlock,parameterBlock} state = dataBlock;
     while (!finished) {
-        switch (state) {
-        case readLine:
-        {
-            QString temp = stream.readLine();
-            if (temp.isEmpty())
-                finished = true;
-            line += temp;
-            state = process;
-            break;
+        int idx1 = line.indexOf('%');
+        int idx2 = line.indexOf('*');
+        if ((idx2 != -1) && ((idx2 < idx1) || (idx1 == -1))) {
+            // complete data block found
+            QString block = line.left(idx2+1);
+            line.remove( 0, idx2+1 );
+            if (state == dataBlock)
+                processDataBlock( block );
+            else
+                processParameterBlock( block );
+            continue;
         }
-        case process:
-        {
-            int idx1 = line.indexOf('%');
-            int idx2 = line.indexOf('*');
-            if ((idx2 != -1) && ((idx2 < idx1) || (idx1 == -1))) {
-                // complete data block found
-                QString dataBlock = line.left(idx2+1);
-                line.remove( 0, idx2+1 );
-                processDataBlock( dataBlock );
-                state = process;
-                continue;
-            }
-            if (idx1 != -1) {
-                // start of parameter block found
-                line.remove( 0, idx1+1 );
+        if (idx1 != -1) {
+            // start/end of parameter block found
+            line.remove( 0, idx1+1 ); // remove garbage
+            if (state == dataBlock)
                 state = parameterBlock;
-                continue;
-            }
-            state = readLine;
-            break;
+            else
+                state = dataBlock;
+            continue;
         }
-        case parameterBlock:
-        {
-            int idx1 = line.indexOf('%');
-            if (idx1 != -1) {
-                // end of parameter block found
-                QString parameterBlock = line.left(idx1);
-                line.remove( 0, idx1+1 );
-                processParameterBlock( parameterBlock );
-                state = process;
-                continue;
-            }
-            QString temp = stream.readLine();
-            if (temp.isEmpty())
-                finished = true;
-            line += temp;
-            break;
-        }
-        } // switch
+        QString temp = stream.readLine();
+        if (temp.isEmpty() && stream.atEnd())
+            finished = true;
+        line += temp;
     }
 
     return true;
