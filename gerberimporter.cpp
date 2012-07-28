bc146d217bca363a5db6233fb23ae9bae43d27e2
Sebastian Held <sebastian.held@gmx.de>
2012-07-28 15:23:46 +0200

    more commands; first layer implementation

--- gerberimporter.cpp
+++ gerberimporter.cpp
@@ -1,6 +1,5 @@
 #include "gerberimporter.h"
 
-#include <gmpxx.h>
 #include <QtCore>
 
 GerberImporter::GerberImporter()
@@ -8,6 +7,12 @@ GerberImporter::GerberImporter()
 //    QList<QByteArray> l = QTextCodec::availableCodecs();
 //    foreach (QByteArray name, l)
 //        qDebug() << name;
+    m_deprecated_parameters << "AS" << "MI" << "OF" << "SF" << "IP" << "IR" << "KO" << "SM";
+
+    m_FS_integer = -1;
+    m_FS_decimals = -1;
+    m_FS_zero = omit_leading;
+    m_MO = in;
 }
 
 bool GerberImporter::import( QString filename )
@@ -27,9 +32,10 @@ bool GerberImporter::import( QString filename )
             // complete data block found
             QString block = line.left(idx2+1);
             line.remove( 0, idx2+1 );
-            if (state == dataBlock)
-                processDataBlock( block );
-            else
+            if (state == dataBlock) {
+                if (processDataBlock( block ) == false)
+                    return true;
+            } else
                 processParameterBlock( block );
             continue;
         }
@@ -51,18 +57,171 @@ bool GerberImporter::import( QString filename )
     return true;
 }
 
-void GerberImporter::processDataBlock( QString dataBlock )
+bool GerberImporter::processDataBlock( QString dataBlock )
 {
-    static mpq_class current_x(0);
-    static mpq_class current_y(0);
-
     dataBlock = dataBlock.trimmed();
     qDebug() << "dataBlock:" << dataBlock;
+
+    if (dataBlock.left(3) == "M02")
+        return false; // end of file
+    else if (dataBlock.left(3) == "G04")
+        qDebug() << "Comment:" << dataBlock.mid(3,dataBlock.length()-4);
+
+    return true; // go on with processing
 }
 
 void GerberImporter::processParameterBlock( QString parameterBlock )
 {
     parameterBlock = parameterBlock.trimmed();
     qDebug() << "parameterBlock:" << parameterBlock;
+
+    //parameterBlock = parameterBlock.left(2).toUpper() + parameterBlock.remove(0,2);
+    parameterBlock = parameterBlock.toUpper();
+
+    if (m_deprecated_parameters.contains(parameterBlock.left(2))) {
+        qDebug() << "deprecated Parameter. IGNORING.";
+        return;
+    }
+
+    if (parameterBlock.left(2) == "FS")
+        parameterFS( parameterBlock );
+    else if (parameterBlock.left(2) == "MO")
+        parameterMO( parameterBlock );
+    else if (parameterBlock.left(2) == "LN")
+        parameterLN( parameterBlock );
+}
+
+//! \brief Define the number format.
+void GerberImporter::parameterFS( QString parameterBlock )
+{
+    if (m_FS_integer != -1) {
+        // only one times allowed
+        qDebug() << "FS specified multiple times";
+        return;
+    }
+    if (parameterBlock.mid(2,1) == "L")
+        m_FS_zero = omit_leading;
+    else if (parameterBlock.mid(2,1) == "T")
+        m_FS_zero = omit_trailing;
+    else {
+        qDebug() << "FS error";
+        return;
+    }
+    if (parameterBlock.mid(3,1) == "A")
+        ;
+    else if (parameterBlock.mid(3,1) == "I") {
+        qDebug() << "FS I unsupported";
+        return;
+    } else {
+        qDebug() << "FS error";
+        return;
+    }
+    int pos = 4;
+    if (parameterBlock.mid(pos,1) == "N") {
+        qDebug() << "FS N unsupported";
+        pos += 3;
+    }
+    if (parameterBlock.mid(pos,1) == "G") {
+        qDebug() << "FS G unsupported";
+        pos += 3;
+    }
+    if (parameterBlock.mid(pos,1) == "X") {
+        bool ok;
+        int temp = parameterBlock.mid(++pos,1).toInt(&ok);
+        if (ok)
+            m_FS_integer = temp;
+        temp = parameterBlock.mid(++pos,1).toInt(&ok);
+        if (ok)
+            m_FS_decimals = temp;
+        pos++;
+    } else {
+        qDebug() << "FS error";
+        return;
+    }
+    if (parameterBlock.mid(pos,1) == "Y") {
+        bool ok;
+        int temp = parameterBlock.mid(++pos,1).toInt(&ok);
+        if (!ok || (m_FS_integer != temp)) {
+            qDebug() << "FS Y error";
+            return;
+        }
+        temp = parameterBlock.mid(++pos,1).toInt(&ok);
+        if (!ok || (m_FS_decimals != temp)) {
+            qDebug() << "FS Y error";
+            return;
+        }
+        pos++;
+    } else {
+        qDebug() << "FS error";
+        return;
+    }
+}
+
+//! \brief set mode inches or millimeters
+void GerberImporter::parameterMO( QString parameterBlock )
+{
+    if (parameterBlock.mid(2,2) == "MM")
+        m_MO = mm;
+    else if (parameterBlock.mid(2,2) == "IN")
+        m_MO = in;
+    else {
+        qDebug() << "MO error";
+        return;
+    }
+}
+
+//! \brief Name a layer.
+void GerberImporter::parameterLN( QString parameterBlock )
+{
+    newLayer().setName( parameterBlock.mid(2,parameterBlock.length()-3) );
+    qDebug() << "The layer's name is:" << newLayer().name();
+}
+
+Layer& GerberImporter::newLayer()
+{
+    if (m_layers.isEmpty())
+        m_layers << Layer();
+    if (!m_layers.last().isEmpty())
+        m_layers << Layer();
+
+    return m_layers.last();
+}
+
+Layer& GerberImporter::currentLayer()
+{
+    if (m_layers.isEmpty())
+        m_layers << Layer();
+
+    return m_layers.last();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Layer::Layer()
+{
+    m_current_x = 0;
+    m_current_y = 0;
+    m_imagePolarity = positive;
+
+}
+
+Layer::~Layer()
+{
 
 }
--- gerberimporter.h
+++ gerberimporter.h
@@ -2,6 +2,30 @@
 #define GERBERIMPORTER_H
 
 #include <QString>
+#include <QStringList>
+#include <gmpxx.h>
+
+class Layer
+{
+public:
+    enum ImagePolarity {positive, negative};
+public:
+    Layer();
+    ~Layer();
+
+    void setImagePolarity( ImagePolarity p ) {m_imagePolarity = p;}
+    ImagePolarity imagePolarity() const {return m_imagePolarity;}
+    void setName( QString name ) {m_name = name;}
+    QString name() const {return m_name;}
+    bool isEmpty() const {return m_objects.isEmpty();}
+
+protected:
+    mpq_class m_current_x;
+    mpq_class m_current_y;
+    ImagePolarity m_imagePolarity;
+    QString m_name;
+    QList<bool> m_objects;
+};
 
 class GerberImporter
 {
@@ -11,8 +35,23 @@ public:
     bool import( QString filename );
 
 protected:
-    void processDataBlock( QString dataBlock );
+    bool processDataBlock( QString dataBlock );
     void processParameterBlock( QString parameterBlock );
+    void parameterFS( QString parameterBlock );
+    void parameterMO( QString parameterBlock );
+    void parameterLN( QString parameterBlock );
+
+    Layer& newLayer();
+    Layer& currentLayer();
+
+    QStringList m_deprecated_parameters;
+
+    QList<Layer> m_layers;
+
+    // current graphics state
+    int m_FS_integer, m_FS_decimals;
+    enum {omit_leading, omit_trailing} m_FS_zero;
+    enum {mm,in} m_MO;
 };
 
 #endif // GERBERIMPORTER_H
